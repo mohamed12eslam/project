f075feb51f87dbb1a6779afc8ac2b670260527d4
mohamed12eslam <mohamedeslam2000m@gmail.com>
2025-04-23 00:56:54 +0200

    اول رفع للمشروع

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+int age=0;
+string name="mohamed eslam";
+char grade='B';
+double gpa=0;
+bool success=  true;
+
+
+cout<< "enter your name "<<endl;
+cin>>name;
+
+cout<<"enter your age "<<endl;
+cin>>age;
+
+cout<<"enter your grade "<<endl;
+cin>>grade;
+
+cout<<"enter your GPA" <<endl;
+cin>>gpa;
+
+cout<<"you are success ? Answer with true or fulse"<<endl;
+cin>>success;
+cout<<"==================================================="<<endl;
+
+
+cout<<"your name is "<<name<<endl<<endl<<endl;
+cout<<"your age is "<<age<<endl<<endl<<endl;
+cout<<"your grade is "<<grade<<endl<<endl<<endl;
+cout<<"your GPA is " <<gpa<<endl<<endl<<endl;
+
+
+cout<<"if success =1 congratulations else you will do it agine "<<success<<endl<<endl<<endl;
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+
